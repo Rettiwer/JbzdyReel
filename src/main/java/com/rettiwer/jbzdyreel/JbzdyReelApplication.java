eee7bf99aec1006ab743224076097c18edbdfc85
Rettiwer <rettiwerpytania@gmail.com>
2022-07-21 22:21:48 +0200

    Let's scrape some meme posts

--- src/main/java/com/rettiwer/jbzdyreel/JbzdyReelApplication.java
+++ src/main/java/com/rettiwer/jbzdyreel/JbzdyReelApplication.java
@@ -1,13 +1,35 @@
 package com.rettiwer.jbzdyreel;
 
+import com.rettiwer.jbzdyreel.scrapebot.ScrapeBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@EnableAsync
 public class JbzdyReelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JbzdyReelApplication.class, args);
     }
 
+    @Bean
+    public TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+
+    @Bean
+    public CommandLineRunner schedulingRunner(TaskExecutor executor) {
+        return args -> executor.execute(new ScrapeBot());
+    }
 }
