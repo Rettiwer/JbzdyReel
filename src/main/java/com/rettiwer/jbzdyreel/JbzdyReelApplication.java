0bc7378631c81f96e91064f6b2a51912dacd72b0
Rettiwer <rettiwerpytania@gmail.com>
2022-07-21 08:50:08 +0200

    Initial commit

---
+++ src/main/java/com/rettiwer/jbzdyreel/JbzdyReelApplication.java
@@ -0,0 +1,13 @@
+package com.rettiwer.jbzdyreel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbzdyReelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbzdyReelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rettiwer/jbzdyreel/JbzdyReelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rettiwer.jbzdyreel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbzdyReelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
